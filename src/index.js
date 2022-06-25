2f02f9c0e8e7d5864203c70035f563669a19ab9c
Markin Petr <skyhome5041@icloud.com>
2022-06-26 00:52:09 +0700

    add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let numberToString = String(n);
+  let output = '';
+  if (numberToString[0] === '-') {
+    output = numberToString.slice(1);
+    output = output.split('').reverse().join('');
+  } else {
+    output += numberToString.split('').reverse().join('');
+  }
+  let result = +output;
+  return (result);
 }
